9fad7f6e1e0292a6d7f1d093efa9a21e442b9ac6
Rob Griffin <117731652+griffindatasci@users.noreply.github.com>
2023-06-29 20:12:25 +0200

    output resolved for dim_films; was materializing as a view despite spec otherwise - config in .sql file fixed it

--- swapi_2_0_venv/swapi_2_0_dbt/models/dim_films.sql
+++ swapi_2_0_venv/swapi_2_0_dbt/models/dim_films.sql
@@ -1,3 +1,6 @@
+{{ config(materialized='table') }}
+
+
 with films as (
     select title, episode_id, opening_crawl, director, producer, release_date, url, created, edited 
     from {{source('postgres', 'stg_films')}}
